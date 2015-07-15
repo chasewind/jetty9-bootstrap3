5b3ca4421a2c09392d10ee4c406146753c39bd9b
yudongwei <yudw@kunion.com.cn>
2015-07-15 14:28:30 +0800

    login page

--- src/test/java/com/belief/test/AppTest.java
+++ src/test/java/com/belief/test/AppTest.java
@@ -1,33 +1,34 @@
 package com.belief.test;
 
 public class AppTest {
-    public static void main(String[] args) {
-        String[] array =
-                "ad-products|http://img.kcyouli.com|iPhkRNUl5GwPOIs-yGElVYb2PrWsBqWIqmUZMpsv|p4_noUMIvnfWxxzhkUwMWsrwd-9hB0u2CFbQalIS"
-                        .split("\\|");
-        for (String str : array) {
-            System.out.println(str);
-        }
-        array =
-                "kk-resource|http://kk-resource.qiniudn.com|pvq0yhV90DZRWntM0jZLI_KkSgpwT2rWg6NUDNkT|iGExckccKW6zoh67BKq0etiYB2bZ6gPS0_xNAKEX"
-                        .split("\\|");
-        for (String str : array) {
-            System.out.println(str);
-        }
-        // ////////////////////
-        System.out.println("-------------------");
-        String fileName = "hello.txt";
-        int idx = fileName.lastIndexOf(".");
-        // 获取后缀
-        String suffix = fileName.substring((idx + 1));
-        System.out.println(suffix);
-        System.out.println("-------------------");
-        double ratio = 1.66666666;
-        System.out.println(Math.ceil(ratio));
-        System.out.println(Math.floor(ratio));
-        ratio = 1.222222222;
-        System.out.println(Math.ceil(ratio));
-        System.out.println(Math.floor(ratio));
-
-    }
+	public static void main(String[] args) {
+		String[] array = "ad-products|http://img.kcyouli.com|iPhkRNUl5GwPOIs-yGElVYb2PrWsBqWIqmUZMpsv|p4_noUMIvnfWxxzhkUwMWsrwd-9hB0u2CFbQalIS".split("\\|");
+		for (String str : array) {
+			System.out.println(str);
+		}
+		array = "kk-resource|http://kk-resource.qiniudn.com|pvq0yhV90DZRWntM0jZLI_KkSgpwT2rWg6NUDNkT|iGExckccKW6zoh67BKq0etiYB2bZ6gPS0_xNAKEX".split("\\|");
+		for (String str : array) {
+			System.out.println(str);
+		}
+		// ////////////////////
+		System.out.println("-------------------");
+		String fileName = "hello.txt";
+		int idx = fileName.lastIndexOf(".");
+		// 获取后缀
+		String suffix = fileName.substring((idx + 1));
+		System.out.println(suffix);
+		System.out.println("-------------------");
+		double ratio = 1.66666666;
+		System.out.println(Math.ceil(ratio));
+		System.out.println(Math.floor(ratio));
+		ratio = 1.222222222;
+		System.out.println(Math.ceil(ratio));
+		System.out.println(Math.floor(ratio));
+		System.out.println("-------------------");
+		int count = 14;
+		int rows = 20;
+		int result = count % rows == 0 ? count / rows : (count / rows) + 1;
+		System.out.println(result);
+		System.out.println(count / rows);
+	}
 }
